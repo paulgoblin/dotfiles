6b8bff12caa9b62de79953a3032cf4f0f0a0a707
Paul Richter <michaelrichter@ebay.com>
2018-01-03 10:54:14 -0800

    Add bash extension

--- aliases.bash
+++ aliases.bash
@@ -6,6 +6,8 @@ alias sbrc='source ~/.bash_profile'
 # git
 alias ga='git add'
 alias gc='git commit'
+alias gca='git commit --amend'
+alias gcnv='git commit --no-verify'
 alias gs='git status'
 alias gl='git log'
 alias gd='git diff'
